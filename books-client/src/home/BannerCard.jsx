a44d97fb221b18a7e4738a03ad647ba9c92297c2
ranveer <ranveersinghsingh2644@gmail.com>
2024-08-05 22:46:05 +0530

    Corrected front page

--- books-client/src/home/BannerCard.jsx
+++ books-client/src/home/BannerCard.jsx
@@ -1,15 +1,18 @@
-import React, { useRef, useState } from 'react';
-// Import Swiper React components
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/effect-cards';
-
 import './BannerCard.css';
-
-// import required modules
 import { EffectCards } from 'swiper/modules';
+
+// Import images directly
+import book1 from '../assets/banner-books/book1.png';
+import book2 from '../assets/banner-books/book2.png';
+import book3 from '../assets/banner-books/book3.png';
+import book4 from '../assets/banner-books/book4.png';
+import book5 from '../assets/banner-books/book5.png';
+
+const images = [book1, book2, book3, book4, book5];
 
 function BannerCard() {
   return (
@@ -20,14 +23,14 @@ function BannerCard() {
         modules={[EffectCards]}
         className="mySwiper"
       >
-        <SwiperSlide></SwiperSlide>
-        <SwiperSlide></SwiperSlide>
-        <SwiperSlide></SwiperSlide>
-        <SwiperSlide></SwiperSlide>
-        <SwiperSlide></SwiperSlide>
+        {images.map((image, index) => (
+          <SwiperSlide key={index}>
+            <img src={image} alt={`Book ${index + 1}`} />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
-  )
+  );
 }
 
-export default BannerCard
+export default BannerCard;
